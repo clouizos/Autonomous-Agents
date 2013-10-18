d70c6ebdc86bb22a43b8eecc5da1d1da5897c983
Christos Louizos <chr.louizos@gmail.com>
2013-10-18 14:29:18 +0200

    updated python script

--- Assignment3/testoutput/parse.py
+++ Assignment3/testoutput/parse.py
@@ -42,6 +42,7 @@ for i in range(nrfiles):
     gamma[i] = tokens[2]
     nr_preds[i] = tokens[3]
     nr_runs[i] = tokens[4]
+  
     method[i] = tokens[5]
     actionselect[i] = tokens[6]
     # read data in
@@ -51,31 +52,35 @@ for i in range(nrfiles):
     data[i] = data[i][0:int(nr_runs[i]):1]
     for j in range(len(data[i])):
         data[i][j] = float(data[i][j])
+    averaging_step = 100
     # Averaging
-    ddata[i] = range(int(nr_runs[i])/100)
-    for k in range(len(data[i])/100):
-        ddata[i][k] = sum(data[i][k*100:k*100+100])/100
+    ddata[i] = range(int(nr_runs[i])/averaging_step)
+    for k in range(len(data[i])/averaging_step):
+        ddata[i][k] = sum(data[i][k*averaging_step:k*averaging_step + averaging_step])/averaging_step
     # Plotting
-    #print data
+    step_to_show = 20
     plt.figure(1)
-    plt.plot(ddata[i][80:100], colors[i], label = alpha[i])
-    plt.ylim([0,50])
+    plt.plot(ddata[i][len(ddata[i]) - step_to_show:len(ddata[i])], colors[i], label = alpha[i])
+    #plt.plot(data[i][len(data[i]) - step_to_show:len(data[i])], colors[i], label = alpha[i])
+    #plt.ylim([0,50])
     plt.ylabel('episode size')
-    plt.xlabel('last 20 iterations x 100')
+    plt.xlabel('last 200 iterations')
     plt.legend()    
     
     plt.figure(2)
     plt.plot(ddata[i], colors[i], label = alpha[i])
-    plt.ylim([0,500])
+    #plt.plot(data[i], colors[i], label = alpha[i])
+    #plt.ylim([0,500])
     plt.ylabel('episode size')
-    plt.xlabel('iterations x 100')
+    plt.xlabel('iterations')
     plt.legend()
     
     plt.figure(3)
-    plt.plot(ddata[i][0:20], colors[i], label = alpha[i])
-    plt.ylim([0,500])
+    plt.plot(ddata[i][0:step_to_show], colors[i], label = alpha[i])
+    #plt.plot(data[i][0:step_to_show], colors[i], label = alpha[i])
+    #plt.ylim([0,500])
     plt.ylabel('episode size')
-    plt.xlabel('first 20 iterations x 100')
+    plt.xlabel('first 20 iterations')
     plt.legend()
 
 plt.show()
